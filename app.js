9737be1a0bd0701c4d68e1ac693991018085ab27
Isaac <isaac.ewing3@gmail.com>
2021-05-05 16:21:40 -0600

    Added the reset button and the counter for it

--- app.js
+++ app.js
@@ -1,16 +1,20 @@
 import { didUserWin } from './functions.js';
+import { getRandomThrow } from './get-random-throw.js';
 
 const userGuess = document.getElementsByName('r-p-s');
 const gameResult = document.getElementById('game-result');
-const totalResult = document.getElementById('total-result');
+const totalResets = document.getElementById('total-resets');
 const submitButton = document.getElementById('button');
+const resetButton = document.getElementById('reset');
 
 let userThrow = 0;
 let wins = 0;
 let losses = 0;
+let draws = 0;
+let resets = 0;
 
 submitButton.addEventListener('click', () => {
-    let compThrow = Math.ceil(Math.random() * 3);
+    let compThrow = getRandomThrow();
     if (userGuess[0].checked)
         userThrow = userGuess[0].value;
     if (userGuess[1].checked)
@@ -26,13 +30,23 @@ submitButton.addEventListener('click', () => {
         compThrow = "scissors"
     if  (currentResult === 1) {
         wins++;
-        gameResult.textContent = `You win! Your opponent threw ${compThrow}. Wins: ${wins} Losses: ${losses}`
+        gameResult.textContent = `You win! Your opponent threw ${compThrow}. Wins: ${wins} Losses: ${losses} Draws: ${draws}`
     }
     if  (currentResult === 0) {
-        gameResult.textContent = `You tied. Your opponent threw ${compThrow}. Wins: ${wins} Losses: ${losses}`
+        draws++;
+        gameResult.textContent = `You tied. Your opponent threw ${compThrow}. Wins: ${wins} Losses: ${losses} Draws: ${draws}`
     }
     if  (currentResult === -1) {
         losses++;
-        gameResult.textContent = `You lose :( Your opponent threw ${compThrow}. Wins: ${wins} Losses: ${losses}`
+        gameResult.textContent = `You lose :( Your opponent threw ${compThrow}. Wins: ${wins} Losses: ${losses} Draws: ${draws}`
     }
 });
+
+resetButton.addEventListener('click', () => {
+    wins = 0;
+    losses = 0;
+    draws = 0;
+    resets++;
+    gameResult.textContent = `Wins: ${wins} Losses: ${losses} Draws: ${draws}`
+    totalResets.textContent = `Resets: ${resets}`;
+})
---
+++ get-random-throw.js
@@ -0,0 +1,4 @@
+export function getRandomThrow() {
+    let rThrow = Math.ceil(Math.random() * 3);
+    return (rThrow);
+}
