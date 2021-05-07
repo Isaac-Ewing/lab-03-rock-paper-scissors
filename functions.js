461bda05c726a986325aad0a01e0a8f5c12103ac
BenFrancka <benfrancka@gmail.com>
2021-05-07 13:59:10 -0700

    reworked computer throw and didUserWin functions

--- functions.js
+++ functions.js
@@ -1,4 +1,12 @@
 export function didUserWin(user, computer) {
+
+    if (computer === 'rock')
+        computer = 1;
+    if (computer === 'paper')
+        computer = 2;
+    if (computer === 'scissors')
+        computer = 3;
+
     if (user - computer === -2)
         return ('win');
     if (user - computer === 2)
@@ -9,16 +17,8 @@ export function didUserWin(user, computer) {
         return ('lose');
     if ((user - computer) === 0)
         return ('draw');
-
-    /*if (user - computer === -2)
-        return ('win');
-    if (user - computer === 2)
-        return (-1);
-    if ((user - computer) > 0)
-        return (1);
-    if ((user - computer) < 0)
-        return (-1);
-    if ((user - computer) === 0)
-        return (0);*/
 }
+
+
+
 
