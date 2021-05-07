e350a9493b05737558598b8b9f4d549bfc83d1b5
BenFrancka <benfrancka@gmail.com>
2021-05-07 14:04:34 -0700

    changed test files to reflect new functions

--- test-user-win.js
+++ test-user-win.js
@@ -3,31 +3,31 @@ import { didUserWin } from './functions.js';
 const test = QUnit.test;
 
 test('time to test a function', (expect) => {
-    const expected = 1;
+    const expected = 'rock';
     const actual = didUserWin(1, 3);
     expect.equal(actual, expected);
 });
 
 test('time to test a function', (expect) => {
-    const expected = 0;
+    const expected = 'draw';
     const actual = didUserWin(3, 3);
     expect.equal(actual, expected);
 });
 
 test('time to test a function', (expect) => {
-    const expected = -1;
+    const expected = 'lose';
     const actual = didUserWin(3, 1);
     expect.equal(actual, expected);
 });
 
 test('time to test a function', (expect) => {
-    const expected = 1;
+    const expected = 'lose';
     const actual = didUserWin(3, 2);
     expect.equal(actual, expected);
 });
 
 test('time to test a function', (expect) => {
-    const expected = -1;
+    const expected = 'lose';
     const actual = didUserWin(2, 3);
     expect.equal(actual, expected);
 });
