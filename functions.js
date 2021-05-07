e9508d37d538faa280c442f8e5f7f019210907d1
BenFrancka <benfrancka@gmail.com>
2021-05-07 13:44:03 -0700

    reworked didUserWinFunction

--- functions.js
+++ functions.js
@@ -1,6 +1,17 @@
 export function didUserWin(user, computer) {
     if (user - computer === -2)
-        return (1);
+        return ('win');
+    if (user - computer === 2)
+        return ('lose');
+    if ((user - computer) > 0)
+        return ('win');
+    if ((user - computer) < 0)
+        return ('lose');
+    if ((user - computer) === 0)
+        return ('draw');
+
+    /*if (user - computer === -2)
+        return ('win');
     if (user - computer === 2)
         return (-1);
     if ((user - computer) > 0)
@@ -8,5 +19,6 @@ export function didUserWin(user, computer) {
     if ((user - computer) < 0)
         return (-1);
     if ((user - computer) === 0)
-        return (0);
+        return (0);*/
 }
+
